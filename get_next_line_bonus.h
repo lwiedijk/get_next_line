965b0d025ec4a11ea3aaaddf390740bd6c4c2c1f
lwiedijk <67950116+lwiedijk@users.noreply.github.com>
2021-02-13 17:13:29 +0100

    add bonus header file

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line_bonus.h                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/12/12 12:18:11 by lwiedijk      #+#    #+#                 */
+/*   Updated: 2021/01/13 13:29:54 by lwiedijk      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <stddef.h>
+
+int		get_next_line(int fd, char **line);
+void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strdup(const char *s1);
+
+#endif
